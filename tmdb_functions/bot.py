044cf97900593765d7020c295c9a00f068d08242
Fiorella Barrientos Villalta <fifii.geral@gmail.com>
2019-10-17 16:01:20 -0500

    Create bot.py

---
+++ tmdb_functions/bot.py
@@ -0,0 +1,20 @@
+import re
+import requests
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+token_ = '857019165:AAHkHPXfVU-iw6yb7EP5GOtQzXz4LJ8h03k'
+
+
+def start(update, context):
+    update.message.reply_text(
+        'Hola! Soy MovieGram bot. Estoy aqui para ayudarte en tus aventuras cinefilas')
+
+
+def main():
+    updater = Updater(token_, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
